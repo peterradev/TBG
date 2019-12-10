ca9bb81a0a5d3ce3de9deb21d5fe1e14322437ae
peter <pradev@luc.edu>
2019-12-09 18:51:39 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const express = require('express');
+
+const port = proccess.env.PORT || 3030;
+
+const publicDir = path.join(__dirname, '../public');
+
+var app = express();
+
+app.use(express.static(publicDir));
+
+// app.listen(3030, ()=>{
+//     console.log('server running on port 3030');
+// });
+
+app.listen(port, ()=>{
+    console.log('server running on port ${port}');
+});
+
